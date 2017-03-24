76a826b1df08d1d3688b39f9428cecb0259d5a04
nomilous <nomilous@gmail.com>
2017-03-24 14:41:08 +0200

    emit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # happner-cluster-info
+
 Demo
+
+see [happner-cluster-demo](https://github.com/happner/happner-cluster-demo)
---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = Info;
+
+
+function Info() {
+  this.interval = null;
+}
+
+Info.prototype.start = function ($happn, callback) {
+
+  if (!$happn._mesh) {
+    return callback (new Error($happn.name + ' requires accessLevel: mesh'));
+  }
+
+  this.interval = setInterval(function () {
+
+    this.__emitInfo($happn);
+
+  }.bind(this), 1000);
+
+  callback();
+
+};
+
+
+Info.prototype.stop = function ($happn, callback) {
+
+  clearInterval(this.interval);
+
+  callback();
+
+};
+
+
+Info.prototype.__emitInfo = function ($happn) {
+  $happn.log.info('emit');
+};
